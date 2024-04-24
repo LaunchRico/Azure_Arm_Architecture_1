255af7b62025faf6f045d8061e7668a80303624f
manuel martin rico <manuelmartinrico@manuels-MacBook-Pro.local>
2024-04-23 22:10:26 -0600

    Coding Queue query system

--- dev/function/QueueTrigger.cs
+++ dev/function/QueueTrigger.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
 namespace function
@@ -8,9 +9,34 @@ namespace function
     public class QueueTrigger
     {
         [FunctionName("QueueTrigger")]
-        public void Run([QueueTrigger("queuemmrsboxwestus001", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("queuemmrsboxwestus001", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            var connectionString = Environment.GetEnvironmentVariable("SqlConnection");
+
+            using (SqlConnection conn = new SqlConnection(connectionString)) {
+                conn.Open();
+                var newTableName = "TestTable";
+
+                var createTableCmdText = $@"
+                    IF NOT EXISTS (
+                        SELECT * 
+                        FROM INFORMATION_SCHEMA.TABLES 
+                        WHERE TABLE_SCHEMA = 'dbo' 
+                        AND TABLE_NAME = '{newTableName}')
+                    BEGIN
+                        CREATE TABLE dbo.{newTableName} (
+                            ID INT IDENTITY(1,1) PRIMARY KEY,
+                            Column1 NVARCHAR(MAX)
+                        )
+                    END";
+
+                using (SqlCommand createTableCMD = new SqlCommand(createTableCmdText, conn))
+                {
+                    createTableCMD.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
