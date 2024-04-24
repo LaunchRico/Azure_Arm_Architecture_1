9e5c15481c5dff145ca8fc27cad5284cda32943e
manuel martin rico <manuelmartinrico@manuels-MacBook-Pro.local>
2024-04-23 22:12:11 -0600

    adding more storage strategy

--- main.sh
+++ main.sh
@@ -27,29 +27,38 @@ adminUserName="mmradministrator"
 adminLoginPassword="alBjFg8nen"
 sqlDatabaseName="db-mmr-sbox-westus-001"
 
-#az group create --name $resourceGroup --location $location --tags owner=$owner
+az group create --name $resourceGroup --location $location --tags owner=$owner
 
 #Execute networkin json file
-#az deployment group create --resource-group $resourceGroup --template-file ./network/armTemplate.json --parameters vnetName=$vnetName vnetAddressPrefix=$vnetAddressPrefix privateSubnetName=$privateSubnetName privateSubnetAddressPrefix=$privateSubnetAddressPrefix location=$location serverFarmSubnetName=$serverFarmSubnetName serverFarmSubnetAddressPrefix=$serverFarmSubnetAddressPrefix
+az deployment group create --resource-group $resourceGroup --template-file ./network/armTemplate.json --parameters vnetName=$vnetName vnetAddressPrefix=$vnetAddressPrefix privateSubnetName=$privateSubnetName privateSubnetAddressPrefix=$privateSubnetAddressPrefix location=$location serverFarmSubnetName=$serverFarmSubnetName serverFarmSubnetAddressPrefix=$serverFarmSubnetAddressPrefix
 
 #Execute storage json file
 az deployment group create --resource-group $resourceGroup --template-file ./storage/armTemplate.json --parameters location=$location storageAccountName=$storageAccountName queueName=$queueName sqlServerName=$sqlServerName adminUsername=$adminUserName administratorLoginPassword=$adminLoginPassword sqlDatabaseName=$sqlDatabaseName virtualNetworkName=$vnetName serverFarmSubnetName=$serverFarmSubnetName
 
 #Execute app json file
-#az deployment group create --resource-group $resourceGroup --template-file ./app/armTemplate.json --parameters location=$location webAppName=$webAppName appServicePlan=$appServicePlan functionAppName=$functionAppName functionStorageAccount=$storageAccountName vnetName=$vnetName subnetName=$serverFarmSubnetName
+az deployment group create --resource-group $resourceGroup --template-file ./app/armTemplate.json --parameters location=$location webAppName=$webAppName appServicePlan=$appServicePlan functionAppName=$functionAppName functionStorageAccount=$storageAccountName vnetName=$vnetName subnetName=$serverFarmSubnetName
 
 #Add Application settings
-#az webapp config appsettings set --name $webAppName --resource-group $resourceGroup --settings AzureWebJobsStorage="DefaultEndpointsProtocol=https;AccountName=$storageAccountName;AccountKey=$(az storage account keys list --resource-group $resourceGroup --account-name $storageAccountName --query [0].value -o tsv);EndpointSuffix=core.windows.net"
+az webapp config appsettings set --name $webAppName --resource-group $resourceGroup --settings AzureWebJobsStorage="DefaultEndpointsProtocol=https;AccountName=$storageAccountName;AccountKey=$(az storage account keys list --resource-group $resourceGroup --account-name $storageAccountName --query [0].value -o tsv);EndpointSuffix=core.windows.net"
+
+#Adding connection string to Azure function
+tempVariable=$(az sql db show-connection-string --server $sqlServerName --name $sqlDatabaseName --auth-type SqlPassword --client ado.net)
+
+databaseConnectionString=${tempVariable/<username>/$adminUserName}
+databaseConnectionString=${databaseConnectionString/<password>/$adminLoginPassword}
+
+#Assigning connection string to azure function
+az webapp config connection-string set --name $functionAppName --resource-group $resourceGroup --settings "SqlConnection=$databaseConnectionString" --connection-string-type SQLAzure
 
 #Retriving publish profile
-#publishProfile=$(az webapp deployment list-publishing-profiles --name $webAppName --resource-group $resourceGroup --xml)
-#echo "$publishProfile" | gh secret set $GHSecret --repo $repoOwner/$repoName
+publishProfile=$(az webapp deployment list-publishing-profiles --name $webAppName --resource-group $resourceGroup --xml)
+echo "$publishProfile" | gh secret set $GHSecret --repo $repoOwner/$repoName
 
 #Retriving function publish profile
-#functionPublishProfile=$(az webapp deployment list-publishing-profiles --name $functionAppName --resource-group $resourceGroup --xml)
-#echo "$functionPublishProfile" | gh secret set $GHFunctionSecret --repo $repoOwner/$repoName
+functionPublishProfile=$(az webapp deployment list-publishing-profiles --name $functionAppName --resource-group $resourceGroup --xml)
+echo "$functionPublishProfile" | gh secret set $GHFunctionSecret --repo $repoOwner/$repoName
 
 #Run pipeline
-#gh workflow run $pipelineName --repo $repoOwner/$repoName
+gh workflow run $pipelineName --repo $repoOwner/$repoName
 
-#gh workflow run $functionPipelineName --repo $repoOwner/$repoName
+gh workflow run $functionPipelineName --repo $repoOwner/$repoName
